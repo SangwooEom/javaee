6fb24c72591232fbdc9cc9d5fa8279e793104e28
sangwooeom <cdm19720@gamil.com>
2022-07-04 09:29:39 +0900

    JAVA MAVEN 프로젝트 초기화

---
+++ src/main/java/com/sangwooeom/javaee/json/ObjectModel.java
@@ -0,0 +1,13 @@
+package com.sangwooeom.javaee.json;
+
+import java.io.FileReader;
+
+import jakarta.json.Json;
+import jakarta.json.JsonReader;
+
+public class ObjectModel {
+	public static void main(String[] args) {
+		System.out.println(System.getProperty("user.dir"));
+		//JsonReader reader = Json.createReader(new FileReader())
+	}
+}
